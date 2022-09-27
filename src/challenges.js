d82cc0f31b2bc117e4e7b804e2b579ab896ffd2a
leonardosantosb <leonardosantosbasilio@gmail.com>
2022-09-27 18:58:19 +0100

    desafio 2

--- src/challenges.js
+++ src/challenges.js
@@ -7,8 +7,10 @@ function compareTrue(animal1, animal2) {
 }
 
 // Desafio 2
-function calcArea() {
-  // seu código aqui
+function calcArea(base, height) {
+  let resultado = (base*height)/2
+  return resultado;
+  
 }
 
 // Desafio 3

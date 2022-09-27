46ddd3d882e9fde9c8690bb33cfac299b713da5b
leonardosantosb <leonardosantosbasilio@gmail.com>
2022-09-27 19:01:10 +0100

    desafio 3

--- src/challenges.js
+++ src/challenges.js
@@ -14,8 +14,10 @@ function calcArea(base, height) {
 }
 
 // Desafio 3
-function splitSentence() {
-  // seu código aqui
+function splitSentence(frase) {
+  let resultado = frase.split(" ")
+  return resultado;
+  
 }
 
 // Desafio 4
